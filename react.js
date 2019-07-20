4c631cd0a4eabb33a49a7ae2d8c218a6748992ee
Prashik Deshpande <43670200+PrashikD@users.noreply.github.com>
2019-07-20 09:31:07 +0530

    Initial Commit including basic homepage

---
+++ react.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "10%";
+  document.getElementById("main").style.marginLeft = "2%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "auto";
+  document.body.style.backgroundColor = "#ffe6e6";
+}
+
